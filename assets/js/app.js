d6a5adc50d4c3fac1cbceab32bdcc61821b58e69
Paula Maureira <paumau27@gmail.com>
2024-08-18 04:08:47 -0400

    funciones encriptar y desencriptar ya funcionan, mensaje de prueba desencriptado

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,34 @@
-/*
+function encriptar() {
+  let texto = document.getElementById("textoInicial").value;
 
+  let mensajeInicial = texto
+    .replace(/a/gi, "ai")
+    .replace(/e/gi, "enter")
+    .replace(/i/gi, "imes")
+    .replace(/o/gi, "ober")
+    .replace(/u/gi, "ufat");
+  let nuevoTexto = mensajeInicial;
+  document.write(`<h2>${nuevoTexto}</h2>`);
+}
+
+encriptar();
+
+function desencriptar() {
+  let texto = document.getElementById("textoInicial").value;
+
+  let mensajeInicial = texto
+    .replace(/ai/gi, "a")
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");
+  let nuevoTexto = mensajeInicial;
+  document.write(`<h2>${nuevoTexto}</h2>`);
+}
+desencriptar();
+
+/*
+fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!
 
 
 */
