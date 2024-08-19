0116175a10bf7ff08077b70269d9e1164d1c15fe
Paula Maureira <paumau27@gmail.com>
2024-08-19 07:27:01 -0400

    encriptar ok, desencripar ok, renderiza el mensaje en el div derecho ok, el botón copiar funciona pero renderiza el div completo

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,31 +1,41 @@
 function encriptar() {
   let texto = document.getElementById("textoInicial").value;
 
-  let mensajeInicial = texto
+  //let mensajeInicial = texto
+  return texto
     .replace(/a/gi, "ai")
     .replace(/e/gi, "enter")
     .replace(/i/gi, "imes")
     .replace(/o/gi, "ober")
     .replace(/u/gi, "ufat");
-  let nuevoTexto = mensajeInicial;
-  document.write(`<h2>${nuevoTexto}</h2>`);
 }
+document.getElementById("encriptando").addEventListener("click", function () {
+  let inputText = document.getElementById("textoInicial").value;
+  let textoSalida = encriptar(inputText);
+  document.getElementById("resultDiv").innerHTML = textoSalida;
+});
 
-encriptar();
-
+//funcion para DESENCRIPTAR
 function desencriptar() {
   let texto = document.getElementById("textoInicial").value;
 
-  let mensajeInicial = texto
+  return texto
     .replace(/ai/gi, "a")
     .replace(/enter/gi, "e")
     .replace(/imes/gi, "i")
     .replace(/ober/gi, "o")
     .replace(/ufat/gi, "u");
-  let nuevoTexto = mensajeInicial;
-  document.write(`<h2>${nuevoTexto}</h2>`);
+
+  //let textoSalida = document.write(`<h2>${mensajeInicial}</h2>`);
 }
-desencriptar();
+//desencriptar();
+document
+  .getElementById("desencriptando")
+  .addEventListener("click", function () {
+    let inputText = document.getElementById("textoInicial").value;
+    let textoSalida = desencriptar(inputText);
+    document.getElementById("resultDiv").innerHTML = textoSalida;
+  });
 
 /*
 fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!
